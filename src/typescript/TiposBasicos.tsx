991fb850c8a5978ca9ae77cbf382ec2f10920d7f
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-10-06 21:38:01 -0600

    Adición de arreglos en TiposBasicos.tsx y libros

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -9,25 +9,38 @@ export const TiposBasicos = () => {
     let FicExperiencia = 20;
     let FicActivo : boolean = true;
     let FicIdNombre : string | number = "Chirstian";
+    //let FicPasatiempos : string[] = ['futbol', 'squash', 'leer','series y peliculas'];
+    //FicPasatiempos.push(706);
+    //FicPasatiempos.push(true);
+    let FicLibros = [];
+    FicLibros.push(706);
+    FicLibros.push(true);
+    FicLibros.push('El Principito');
     FicIdNombre = 706;
+    FicIdNombre = "Frank";
+    FicExperiencia = 25;
     return (
         <>
             <h3>Tipos Básicos</h3>  
-            {FicIdNombre},
+            Alias: {FicIdNombre},
             <br></br>
-            {FicNombre},
+            Nombres: {FicNombre},
             <br></br>
-            {FicApellidos},
+            Apellidos: {FicApellidos},
             <br></br>
-            {FicNumControl},
+            No. Control: {FicNumControl},
             <br></br>
-            {FicNumControl2},
+            No. Control 2: {FicNumControl2},
             <br></br>
-            {FicFechaReg.toString()},
+            Fecha de Registro: {FicFechaReg.toString()},
             <br></br>
-            {FicExperiencia},
+            Años de Experiencia: {FicExperiencia},
             <br></br>
-            {(FicActivo) ? 'Activo' : 'No Activo'}
+            Estatus: {(FicActivo) ? 'Activo' : 'No Activo'},
+            <br></br>
+            Libros: {FicLibros[0]},
+            {(FicLibros[1]) ? 'Activo' : 'No Activo'},
+            {FicLibros[2]}
         </>
     )
 }
