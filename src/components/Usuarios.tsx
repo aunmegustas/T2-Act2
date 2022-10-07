34b40a32ca8a9214fe47dea55ffc11c74eaaf416
aunmegustas <cralperezhe@ittepic.edu.mx>
2022-10-07 13:09:46 -0600

    Componente usuarios, adicion de tabla y etiqueta

---
+++ src/components/Usuarios.tsx
@@ -0,0 +1,24 @@
+export const Usuarios = () => {
+    return (
+        <>
+            <div>
+                <h3>Usuarios</h3>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                Avatar
+                            </th>
+                            <th>
+                                Nombre
+                            </th>
+                            <th>
+                                Email
+                            </th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </>
+    )
+}
